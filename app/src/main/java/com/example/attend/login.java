f48d95201a5bcaa1c24f5cef9b0b74f7190186db
Bash <samrobertsb11@gmail.com>
2023-11-24 19:37:27 +0300

    Moved MainActivity code to login

--- app/src/main/java/com/example/attend/login.java
+++ app/src/main/java/com/example/attend/login.java
@@ -18,29 +18,31 @@ import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.w3c.dom.Text;
 
 import java.util.Objects;
 
 public class login extends AppCompatActivity {
-    // declare views
-    TextInputEditText emailInput, passwordInput;
-    Button login;
     private FirebaseAuth mAuth;
-
+    private FirebaseFirestore db;
+    String userType;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // bind views
+        TextInputEditText emailInput = findViewById(R.id.email);
+        TextInputEditText passwordInput = findViewById(R.id.password);
+        Button login = findViewById(R.id.login);
+
         // initialize firebase
         mAuth = FirebaseAuth.getInstance();
-
-        // bind views
-        emailInput = findViewById(R.id.email);
-        passwordInput = findViewById(R.id.password);
-        login = findViewById(R.id.login);
-
-
+        db = FirebaseFirestore.getInstance();
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -55,9 +57,8 @@ public class login extends AppCompatActivity {
                                 if (task.isSuccessful()) {
                                     // Sign in success, update UI with the signed-in user's information
                                     Log.d(TAG, "signInWithEmail:success");
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                    Intent goToMainActivity = new Intent(getApplicationContext(), MainActivity.class);
-                                    startActivity(goToMainActivity);
+
+                                    checkUserType();
                                 } else {
                                     // If sign in fails, display a message to the user.
                                     Log.w(TAG, "signInWithEmail:failure", task.getException());
@@ -74,9 +75,38 @@ public class login extends AppCompatActivity {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
+
         if(currentUser != null){
-            Intent goToMainActivity = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(goToMainActivity);
+            checkUserType();
         }
+    }
+
+    public void checkUserType() {
+        String userId = mAuth.getCurrentUser().getUid();
+
+        DocumentReference usersRef = db.collection("users").document(userId);
+
+        usersRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()) {
+                    DocumentSnapshot doc = task.getResult();
+
+                    userType = doc.getString("userType");
+
+                    if(userType.equals("lecturer")) {
+                        Intent gotoLecActivity = new Intent(getApplicationContext(), lecActivity.class);
+                        startActivity(gotoLecActivity);
+                    }
+                    else if(userType.equals("student")) {
+                        Intent gotoStudentActivity = new Intent(getApplicationContext(), studentActivity.class);
+                        startActivity(gotoStudentActivity);
+                    }
+                }
+                else {
+                    Log.d(TAG, "Error getting document", task.getException());
+                }
+            }
+        });
     }
 }
